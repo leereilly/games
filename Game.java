78f000d5d06a0711f73467c35908cd790254717c
Gurraj Singh <147290984+Gurraj855@users.noreply.github.com>
2023-10-16 02:45:38 -0500

    Create Game.java

---
+++ Game.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int lowerBound = 1;
+        int upperBound = 100;
+        int numberToGuess = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+        int numberOfAttempts = 0;
+        boolean hasGuessedCorrectly = false;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("I have selected a number between " + lowerBound + " and " + upperBound + ". Can you guess it?");
+
+        while (!hasGuessedCorrectly) {
+            System.out.print("Enter your guess: ");
+            int userGuess = scanner.nextInt();
+            numberOfAttempts++;
+
+            if (userGuess < lowerBound || userGuess > upperBound) {
+                System.out.println("Please enter a number between " + lowerBound + " and " + upperBound + ".");
+            } else if (userGuess < numberToGuess) {
+                System.out.println("Too low! Try again.");
+            } else if (userGuess > numberToGuess) {
+                System.out.println("Too high! Try again.");
+            } else {
+                hasGuessedCorrectly = true;
+                System.out.println("Congratulations! You guessed the number in " + numberOfAttempts + " attempts.");
+            }
+        }
+
+        scanner.close();
+    }
+}
